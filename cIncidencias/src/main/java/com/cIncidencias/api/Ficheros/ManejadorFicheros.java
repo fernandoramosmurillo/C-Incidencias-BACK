f8ba84e5eaa3b1f1d5a904ccc5a0c68a75c14fe7
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-04-07 09:32:58 +0200

    Se ha realizado la clase Ficheros y se ha terminado el primer repositorio

---
+++ cIncidencias/src/main/java/com/cIncidencias/api/Ficheros/ManejadorFicheros.java
@@ -0,0 +1,43 @@
+package com.cIncidencias.api.Ficheros;
+import java.io.*;
+
+public class ManejadorFicheros {
+
+    /** Devuelve todo el contenido del archivo en un solo String
+     * 
+     * @param ruta ruta del archivo
+     * @return string con los datos
+     */
+    public static String leer(String ruta) {
+        StringBuilder contenido = new StringBuilder();
+        String linea;
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            while ((linea = br.readLine()) != null) {
+                // Usamos append para ir construyendo el texto
+                contenido.append(linea).append("\n");
+            }
+        } catch (IOException e) {
+            return "Error al leer el archivo: " + e.getMessage();
+        }
+        
+        return contenido.toString(); // Convertimos el constructor a String real
+    }
+
+    /** ESCRIBIR: Guarda un String directamente en el archivo usando PrintWriter
+     * 
+     * @param ruta ruta del archivo
+     * @param texto texto que introduciremos
+     */
+    public static void escribir(String ruta, String texto, boolean sobrescribir) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(ruta,!sobrescribir))) {
+            
+            pw.print(texto);
+            
+            System.out.println("Archivo guardado con éxito.");
+            
+        } catch (IOException e) {
+            System.out.println("Error al abrir el archivo: " + e.getMessage());
+        }
+    }
+}
--- cIncidencias/src/main/java/com/cIncidencias/api/repositorios/UsuarioRepository.java
+++ cIncidencias/src/main/java/com/cIncidencias/api/repositorios/UsuarioRepository.java
@@ -3,10 +3,13 @@ package com.cIncidencias.api.repositorios;
 import com.cIncidencias.api.modelos.Usuario;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -27,11 +30,33 @@ public class UsuarioRepository {
     public String guardarUsuario(Usuario usuario) throws ExecutionException, InterruptedException {
     	
         // Creamos el documento (filas y tabla), si ya existe, entramos en el
-        DocumentReference docRef = firestore.collection("usuarios").document(usuario.getIdUsuario());
+    	DocumentReference docRef = firestore.collection("usuarios").document(usuario.getIdUsuario());
         
         ApiFuture<WriteResult> result = docRef.set(usuario);
         
-        // Retornamos la confirmación junto a la hora en la que se guardó en Google Cloud
-        return "Usuario guardado con éxito en Firestore. Fecha: " + result.get().getUpdateTime();
+        // Hacemos un log de la confirmación junto a la hora en la que se guardó en Google Cloud
+        return "Usuario"+usuario.getNombre()+ " "+usuar con éxito en Firestore. Fecha: " + result.get().getUpdateTime();
     }
+    
+    public List<Usuario> obtenerUsuarios() throws InterruptedException, ExecutionException {
+    	ApiFuture<QuerySnapshot> query = firestore.collection("usuarios").get();
+    	QuerySnapshot querySnapshot = query.get();
+    	return querySnapshot.toObjects(Usuario.class);
+    }
+    
+    public Usuario obtenerUsuario(String idUsuario) throws InterruptedException, ExecutionException {
+    	DocumentReference docRef = firestore.collection("usuarios").document(idUsuario);
+    	ApiFuture<DocumentSnapshot> docSnapshot = docRef.get();
+    	return docSnapshot.get().toObject(Usuario.class);
+    }
+    
+    public void eliminarUsuario(String idUsuario) throws InterruptedException, ExecutionException {
+    	DocumentReference docRef = firestore.collection("usuarios").document(idUsuario);
+    	
+    	ApiFuture<WriteResult> result = docRef.delete();
+    	
+    }
+    
+    
+    
 }
